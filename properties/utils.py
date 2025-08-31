2c4dda5824c1748df2f1e1e870194eb410e1ba53
Daniel Kweku Addo Arku <danielarku438@gmail.com>
2025-08-31 17:10:38 +0000

    Update utils.py

--- properties/utils.py
+++ properties/utils.py
@@ -1,1 +1,21 @@
+from django.core.cache import cache
+from .models import Property
 
+
+def get_all_properties():
+    """
+    Retrieve all properties from cache if available,
+    otherwise fetch from the database and cache them.
+    Cached for 1 hour (3600 seconds).
+    """
+    all_properties = cache.get("all_properties")
+
+    if not all_properties:
+        all_properties = list(
+            Property.objects.all().values(
+                "id", "title", "description", "price", "location", "created_at"
+            )
+        )
+        cache.set("all_properties", all_properties, timeout=3600)
+
+    return all_properties
