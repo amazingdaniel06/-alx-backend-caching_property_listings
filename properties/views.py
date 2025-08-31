cf023198e90db07acec0784582e7b5d841a8f61f
Daniel Kweku Addo Arku <danielarku438@gmail.com>
2025-08-31 17:08:30 +0000

    Update views.py

--- properties/views.py
+++ properties/views.py
@@ -1,1 +1,19 @@
+from django.http import JsonResponse
+from django.views.decorators.cache import cache_page
+from django.utils import timezone
 
+from .models import Property
+
+
+@cache_page(60 * 15)  # cache results for 15 minutes
+def property_list(request):
+    """Return a cached JSON response of all properties."""
+    properties = Property.objects.all().values(
+        "id", "title", "description", "price", "location", "created_at"
+    )
+    data = list(properties)
+
+    return JsonResponse({
+        "properties": data,
+        "cached_at": timezone.now().isoformat()
+    })
