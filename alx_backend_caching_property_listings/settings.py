5fbcfce6133052ee2e72f0b3b47403d309b2c012
Daniel Kweku Addo Arku <danielarku438@gmail.com>
2025-08-31 17:02:26 +0000

    Update settings.py

--- alx_backend_caching_property_listings/settings.py
+++ alx_backend_caching_property_listings/settings.py
@@ -1,1 +1,12 @@
+# alx_backend_caching_property_listings/settings.py
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',  # required
+        'NAME': 'property_db',
+        'USER': 'property_user',
+        'PASSWORD': 'property_password',
+        'HOST': 'postgres',  # matches docker-compose service name
+        'PORT': '5432',      # required
+    }
+}
