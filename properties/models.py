7134e340a4d5626ce1416aadfdd51ce1ad9c07aa
Daniel Kweku Addo Arku <danielarku438@gmail.com>
2025-08-31 17:03:47 +0000

    Update models.py

--- properties/models.py
+++ properties/models.py
@@ -1,1 +1,12 @@
+# properties/models.py
+from django.db import models
 
+class Property(models.Model):
+    title = models.CharField(max_length=200)  # required
+    description = models.TextField()          # required
+    price = models.DecimalField(max_digits=10, decimal_places=2)  # required
+    location = models.CharField(max_length=100)  # required
+    created_at = models.DateTimeField(auto_now_add=True)  # required
+
+    def __str__(self):
+        return f"{self.title} - {self.location}"
